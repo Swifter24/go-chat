a4d203e8b09678c644b50e8a3ac3db70fccc81db
zwz <1915967609@qq.com>
2025-04-28 21:26:21 +0800

    feat : finish basic user service

--- internal/service/gorm/user_info_service.go
+++ internal/service/gorm/user_info_service.go
@@ -1,16 +1,22 @@
 package gorm
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/go-redis/redis/v8"
 	"go_chat/internal/dao"
 	"go_chat/internal/dto/request"
 	"go_chat/internal/dto/respond"
 	"go_chat/internal/model"
+	myredis "go_chat/internal/service/redis"
 	"go_chat/internal/service/sms"
 	"go_chat/pkg/constants"
+	"go_chat/pkg/enum/user_info/user_status_enum"
+	"go_chat/pkg/util/random"
 	"go_chat/pkg/zlog"
 	"gorm.io/gorm"
+	"time"
 )
 
 type userInfoService struct{}
@@ -57,4 +63,224 @@ func (u *userInfoService) Login(loginReq request.LoginRequest) (string, *respond
 // SendSmsCode 发送短信验证码 - 验证码登录
 func (u *userInfoService) SendSmsCode(telephone string) (string, int) {
 	return sms.VerificationCode(telephone)
+}
+
+// Register 注册，返回(message, register_respond_string, error)
+func (u *userInfoService) Register(req request.RegisterRequest) (string, *respond.RegisterRespond, int) {
+	//设置检查key
+	key := "auth_code_" + req.Telephone
+	code, err := myredis.GetKey(key)
+	if err != nil {
+		zlog.Error(err.Error())
+		return constants.SYSTEM_ERROR, nil, -1
+	}
+	if code != req.Telephone {
+		message := "验证码不正确，请重试"
+		zlog.Info(message)
+		return message, nil, -2
+	} else {
+		if err := myredis.DelKeyIfExists(key); err != nil {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, nil, -1
+		}
+	}
+
+	//校验手机号交给前端，这里校验手机号是否已经注册过
+	message, ret := u.checkTelephoneExist(req.Telephone)
+	if ret != 0 {
+		return message, nil, ret
+	}
+	var newUser model.UserInfo
+	newUser.Uuid = "U" + random.GetNowAndLenRandomString(11)
+	newUser.Telephone = req.Telephone
+	newUser.Password = req.Password
+	newUser.Nickname = req.Nickname
+	newUser.Avatar = "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+	newUser.CreatedAt = time.Now()
+	newUser.IsAdmin = u.checkUserIsAdminOrNot(newUser)
+	newUser.Status = user_status_enum.NORMAL
+
+	// 手机号验证，最后一步才调用api，省钱hhh
+	//err := sms.VerificationCode(registerReq.Telephone)
+	//if err != nil {
+	//	zlog.Error(err.Error())
+	//	return "", err
+	//}
+
+	res := dao.GormDB.Create(&newUser)
+	if res.Error != nil {
+		zlog.Error(res.Error.Error())
+		return constants.SYSTEM_ERROR, nil, -1
+	}
+
+	// 注册成功，chat client建立
+	//if err := chat.NewClientInit(c, newUser.Uuid); err != nil {
+	//	return "", err
+	//}
+
+	registerRsp := &respond.RegisterRespond{
+		Uuid:      newUser.Uuid,
+		Telephone: newUser.Telephone,
+		Nickname:  newUser.Nickname,
+		Email:     newUser.Email,
+		Avatar:    newUser.Avatar,
+		Gender:    newUser.Gender,
+		Birthday:  newUser.Birthday,
+		Signature: newUser.Signature,
+		IsAdmin:   newUser.IsAdmin,
+		Status:    newUser.Status,
+	}
+	year, month, day := newUser.CreatedAt.Date()
+	registerRsp.CreatedAt = fmt.Sprintf("%d.%d.%d", year, month, day)
+
+	return "注册成功", registerRsp, 0
+}
+
+func (u *userInfoService) checkTelephoneExist(telephone string) (string, int) {
+	var user model.UserInfo
+	if res := dao.GormDB.First(&user, "telephone = ?", telephone); res.Error != nil {
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			zlog.Info("该电话不存在，可以注册")
+			return "", 0
+		}
+		zlog.Error(res.Error.Error())
+		return constants.SYSTEM_ERROR, -1
+	}
+	zlog.Info("该电话已经存在，注册失败")
+	message := "该电话已经存在，注册失败"
+	return message, -2
+}
+
+// checkUserIsAdminOrNot 检验用户是否为管理员
+func (u *userInfoService) checkUserIsAdminOrNot(user model.UserInfo) int8 {
+	return user.IsAdmin
+}
+
+// SmsLogin 验证码登录
+func (u *userInfoService) SmsLogin(req request.SmsLoginRequest) (string, *respond.LoginRespond, int) {
+	var user model.UserInfo
+	res := dao.GormDB.First(&user, "telephone = ?", req.Telephone)
+	if res.Error != nil {
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			message := "用户不存在，请注册"
+			zlog.Error(message)
+			return message, nil, -2
+		}
+		zlog.Error(res.Error.Error())
+		return constants.SYSTEM_ERROR, nil, -1
+	}
+
+	key := "auth_code_" + req.Telephone
+	code, err := myredis.GetKey(key)
+	if err != nil {
+		zlog.Error(err.Error())
+		return constants.SYSTEM_ERROR, nil, -1
+	}
+	if code != req.SmsCode {
+		message := "验证码不正确，请重试"
+		zlog.Info(message)
+		return message, nil, -2
+	} else {
+		if err := myredis.DelKeyIfExists(key); err != nil {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, nil, -1
+		}
+	}
+
+	loginRsp := &respond.LoginRespond{
+		Uuid:      user.Uuid,
+		Telephone: user.Telephone,
+		Nickname:  user.Nickname,
+		Email:     user.Email,
+		Avatar:    user.Avatar,
+		Gender:    user.Gender,
+		Birthday:  user.Birthday,
+		Signature: user.Signature,
+		IsAdmin:   user.IsAdmin,
+		Status:    user.Status,
+	}
+	year, month, day := user.CreatedAt.Date()
+	loginRsp.CreatedAt = fmt.Sprintf("%d.%d.%d", year, month, day)
+
+	return "登陆成功", loginRsp, 0
+}
+
+// UpdateUserInfo 修改用户信息
+// 某用户修改了信息，可能会影响contact_user_list，不需要删除redis的contact_user_list，timeout之后会自己更新
+// 但是需要更新redis的user_info，因为可能影响用户搜索
+func (u *userInfoService) UpdateUserInfo(updateReq request.UpdateUserInfoRequest) (string, int) {
+	var user model.UserInfo
+	if res := dao.GormDB.First(&user, "uuid = ?", updateReq.Uuid); res.Error != nil {
+		zlog.Error(res.Error.Error())
+		return constants.SYSTEM_ERROR, -1
+	}
+	if updateReq.Email != "" {
+		user.Email = updateReq.Email
+	}
+	if updateReq.Nickname != "" {
+		user.Nickname = updateReq.Nickname
+	}
+	if updateReq.Birthday != "" {
+		user.Birthday = updateReq.Birthday
+	}
+	if updateReq.Signature != "" {
+		user.Signature = updateReq.Signature
+	}
+	if updateReq.Avatar != "" {
+		user.Avatar = updateReq.Avatar
+	}
+	if res := dao.GormDB.Save(&user); res.Error != nil {
+		zlog.Error(res.Error.Error())
+		return constants.SYSTEM_ERROR, -1
+	}
+	//if err := myredis.DelKeysWithPattern("user_info_" + updateReq.Uuid); err != nil {
+	//	zlog.Error(err.Error())
+	//}
+	return "修改用户信息成功", 0
+}
+
+// GetUserInfo 获取用户信息
+func (u *userInfoService) GetUserInfo(uuid string) (string, *respond.GetUserInfoRespond, int) {
+	// redis
+	zlog.Info(uuid)
+	rspString, err := myredis.GetKeyNilIsErr("user_info_" + uuid)
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			zlog.Info(err.Error())
+			var user model.UserInfo
+			if res := dao.GormDB.Where("uuid = ?", uuid).Find(&user); res.Error != nil {
+				zlog.Error(res.Error.Error())
+				return constants.SYSTEM_ERROR, nil, -1
+			}
+			rsp := respond.GetUserInfoRespond{
+				Uuid:      user.Uuid,
+				Telephone: user.Telephone,
+				Nickname:  user.Nickname,
+				Avatar:    user.Avatar,
+				Birthday:  user.Birthday,
+				Email:     user.Email,
+				Gender:    user.Gender,
+				Signature: user.Signature,
+				CreatedAt: user.CreatedAt.Format("2006-01-02 15:04:05"),
+				IsAdmin:   user.IsAdmin,
+				Status:    user.Status,
+			}
+			//rspString, err := json.Marshal(rsp)
+			//if err != nil {
+			//	zlog.Error(err.Error())
+			//}
+			//if err := myredis.SetKeyEx("user_info_"+uuid, string(rspString), constants.REDIS_TIMEOUT*time.Minute); err != nil {
+			//	zlog.Error(err.Error())
+			//}
+			return "获取用户信息成功", &rsp, 0
+		} else {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, nil, -1
+		}
+	}
+	var rsp respond.GetUserInfoRespond
+	if err := json.Unmarshal([]byte(rspString), &rsp); err != nil {
+		zlog.Error(err.Error())
+	}
+	return "获取用户信息成功", &rsp, 0
 }
