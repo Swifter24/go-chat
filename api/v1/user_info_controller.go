69494489ce68612a9b400360bcb3374218b0c315
zwz <1915967609@qq.com>
2025-04-28 21:26:13 +0800

    feat : finish basic user api

--- api/v1/user_info_controller.go
+++ api/v1/user_info_controller.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"go_chat/internal/dto/request"
 	"go_chat/internal/service/gorm"
@@ -33,6 +34,67 @@ func SendSmsCode(c *gin.Context) {
 		})
 		return
 	}
-	message, ret := gorm.UserInfoService.SendSmsCode(req.Telephont)
+	message, ret := gorm.UserInfoService.SendSmsCode(req.Telephone)
 	JsonBack(c, message, ret, nil)
+}
+
+// Register 注册
+func Register(c *gin.Context) {
+	var registerReq request.RegisterRequest
+	if err := c.BindJSON(&registerReq); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	fmt.Println(registerReq)
+	message, userInfo, ret := gorm.UserInfoService.Register(registerReq)
+	JsonBack(c, message, ret, userInfo)
+}
+
+// SmsLogin 验证码登录
+func SmsLogin(c *gin.Context) {
+	var req request.SmsLoginRequest
+	if err := c.BindJSON(&req); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, userInfo, ret := gorm.UserInfoService.SmsLogin(req)
+	JsonBack(c, message, ret, userInfo)
+}
+
+// UpdateUserInfo 修改用户信息
+func UpdateUserInfo(c *gin.Context) {
+	var req request.UpdateUserInfoRequest
+	if err := c.BindJSON(&req); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, ret := gorm.UserInfoService.UpdateUserInfo(req)
+	JsonBack(c, message, ret, nil)
+}
+
+// GetUserInfo 获取用户信息
+func GetUserInfo(c *gin.Context) {
+	var req request.GetUserInfoRequest
+	if err := c.BindJSON(&req); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, userInfo, ret := gorm.UserInfoService.GetUserInfo(req.Uuid)
+	JsonBack(c, message, ret, userInfo)
 }
