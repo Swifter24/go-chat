4e75571a3c48ce0941893f107001eea30b51781b
zwz <1915967609@qq.com>
2025-04-28 21:32:44 +0800

    feat : finish partial group service

---
+++ internal/service/gorm/group_info_service.go
@@ -0,0 +1,186 @@
+package gorm
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"go_chat/internal/dao"
+	"go_chat/internal/dto/request"
+	"go_chat/internal/dto/respond"
+	"go_chat/internal/model"
+	myredis "go_chat/internal/service/redis"
+	"go_chat/pkg/constants"
+	"go_chat/pkg/enum/contact_status_enum"
+	"go_chat/pkg/enum/contact_type_enum"
+	"go_chat/pkg/enum/group_info/group_status_enum"
+	"go_chat/pkg/util/random"
+	"go_chat/pkg/zlog"
+	"time"
+)
+
+type groupInfoService struct {
+}
+
+var GroupInfoService = new(groupInfoService)
+
+func (g *groupInfoService) CreateGroup(groupReq request.CreateGroupRequest) (string, int) {
+	group := model.GroupInfo{
+		Uuid:      fmt.Sprintf("G%s", random.GetNowAndLenRandomString(11)),
+		Name:      groupReq.Name,
+		Notice:    groupReq.Notice,
+		OwnerId:   groupReq.OwnerId,
+		MemberCnt: 1,
+		AddMode:   groupReq.AddMode,
+		Avatar:    groupReq.Avatar,
+		Status:    group_status_enum.NORMAL,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	var members []string
+	members = append(members, groupReq.OwnerId)
+	var err error
+	group.Members, err = json.Marshal(members)
+	if err != nil {
+		zlog.Error(err.Error())
+		return constants.SYSTEM_ERROR, -1
+	}
+	if res := dao.GormDB.Create(&group); res.Error != nil {
+		zlog.Error(res.Error.Error())
+		return constants.SYSTEM_ERROR, -1
+	}
+
+	//添加群主本人
+	contact := model.UserContact{
+		UserId:      groupReq.OwnerId,
+		ContactId:   group.Uuid,
+		ContactType: contact_type_enum.GROUP,
+		Status:      contact_status_enum.NORMAL,
+		CreatedAt:   time.Now(),
+		UpdateAt:    time.Now(),
+	}
+	if res := dao.GormDB.Create(&contact); res.Error != nil {
+		zlog.Error(res.Error.Error())
+		return constants.SYSTEM_ERROR, -1
+	}
+	if err := myredis.DelKeysWithPattern("contact_mygroup_list_" + groupReq.OwnerId); err != nil {
+		zlog.Error(err.Error())
+	}
+	return "创建成功", 0
+}
+
+// LoadMyGroup 获取我创建的群聊
+func (g *groupInfoService) LoadMyGroup(ownerId string) (string, []respond.LoadMyGroupRespond, int) {
+	rspString, err := myredis.GetKeyNilIsErr("contact_mygroup_list_" + ownerId)
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			var groupList []model.GroupInfo
+			if res := dao.GormDB.Order("created_at desc").Where("owner_id = ? ", ownerId).Find(&groupList); res.Error != nil {
+				zlog.Error(res.Error.Error())
+				return constants.SYSTEM_ERROR, nil, -1
+			}
+			var groupListRsp []respond.LoadMyGroupRespond
+			for _, group := range groupList {
+				groupListRsp = append(groupListRsp, respond.LoadMyGroupRespond{
+					GroupId:   group.Uuid,
+					GroupName: group.Name,
+					Avatar:    group.Avatar,
+				})
+			}
+			rspString, err := json.Marshal(groupListRsp)
+			if err != nil {
+				zlog.Error(err.Error())
+			}
+			if err := myredis.SetKeyEx("contact_mygroup_list_"+ownerId, string(rspString), time.Minute*constants.REDIS_TIMEOUT); err != nil {
+				zlog.Error(err.Error())
+			}
+			return "获取成功", groupListRsp, 0
+		} else {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, nil, -1
+		}
+	}
+	var groupListRsp []respond.LoadMyGroupRespond
+	if err := json.Unmarshal([]byte(rspString), &groupListRsp); err != nil {
+		zlog.Error(err.Error())
+	}
+	return "获取成功", groupListRsp, 0
+}
+
+// CheckGroupAddMode 检查群聊加群方式
+func (g *groupInfoService) CheckGroupAddMode(groupId string) (string, int8, int) {
+	rspString, err := myredis.GetKeyNilIsErr("group_info_" + groupId)
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			var group model.GroupInfo
+			if res := dao.GormDB.First(&group, "uuid = ?", groupId); res.Error != nil {
+				zlog.Error(res.Error.Error())
+				return constants.SYSTEM_ERROR, -1, -1
+			}
+			return "加群方式获取成功", group.AddMode, 0
+		} else {
+			zlog.Error(err.Error())
+			return constants.SYSTEM_ERROR, -1, -1
+		}
+	}
+	var rsp respond.GetGroupInfoRespond
+	if err := json.Unmarshal([]byte(rspString), &rsp); err != nil {
+		zlog.Error(err.Error())
+	}
+	return "加群方式获取成功", rsp.AddMode, 0
+}
+
+// EnterGroupDirectly 直接进群
+// ownerId 是群聊id
+func (g *groupInfoService) EnterGroupDirectly(ownerId, contactId string) (string, int) {
+	var group model.GroupInfo
+	if res := dao.GormDB.First(&group, "uuid = ?", ownerId); res.Error != nil {
+		zlog.Error(res.Error.Error())
+		return constants.SYSTEM_ERROR, -1
+	}
+	var members []string
+	if err := json.Unmarshal(group.Members, &members); err != nil {
+		zlog.Error(err.Error())
+		return constants.SYSTEM_ERROR, -1
+	}
+	members = append(members, contactId)
+	if data, err := json.Marshal(members); err != nil {
+		zlog.Error(err.Error())
+		return constants.SYSTEM_ERROR, -1
+	} else {
+		group.Members = data
+	}
+	group.MemberCnt += 1
+	if res := dao.GormDB.Save(&group); res.Error != nil {
+		zlog.Error(res.Error.Error())
+		return constants.SYSTEM_ERROR, -1
+	}
+	newContact := model.UserContact{
+		UserId:      contactId,
+		ContactId:   ownerId,
+		ContactType: contact_type_enum.GROUP,    // 用户
+		Status:      contact_status_enum.NORMAL, // 正常
+		CreatedAt:   time.Now(),
+		UpdateAt:    time.Now(),
+	}
+	if res := dao.GormDB.Create(&newContact); res.Error != nil {
+		zlog.Error(res.Error.Error())
+		return constants.SYSTEM_ERROR, -1
+	}
+	//if err := myredis.DelKeysWithPattern("group_info_" + contactId); err != nil {
+	//	zlog.Error(err.Error())
+	//}
+	//if err := myredis.DelKeysWithPattern("groupmember_list_" + contactId); err != nil {
+	//	zlog.Error(err.Error())
+	//}
+	if err := myredis.DelKeysWithPattern("group_session_list_" + ownerId); err != nil {
+		zlog.Error(err.Error())
+	}
+	if err := myredis.DelKeysWithPattern("my_joined_group_list_" + ownerId); err != nil {
+		zlog.Error(err.Error())
+	}
+	//if err := myredis.DelKeysWithPattern("session_" + ownerId + "_" + contactId); err != nil {
+	//	zlog.Error(err.Error())
+	//}
+	return "进群成功", 0
+}
