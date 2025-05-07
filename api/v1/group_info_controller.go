b6c09acb6ed20d356cd923a31aef51bcff14f09a
zwz <1915967609@qq.com>
2025-05-07 17:56:51 +0800

    feat : finish group api module

--- api/v1/group_info_controller.go
+++ api/v1/group_info_controller.go
@@ -68,3 +68,93 @@ func EnterGroupDirectly(c *gin.Context) {
 	message, ret := gorm.GroupInfoService.EnterGroupDirectly(req.OwnerId, req.ContactId)
 	JsonBack(c, message, ret, nil)
 }
+
+// LeaveGroup 退群
+func LeaveGroup(c *gin.Context) {
+	var req request.LeaveGroupRequest
+	if err := c.BindJSON(&req); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, ret := gorm.GroupInfoService.LeaveGroup(req.UserId, req.GroupId)
+	JsonBack(c, message, ret, nil)
+}
+
+// DismissGroup 解散群聊
+func DismissGroup(c *gin.Context) {
+	var req request.DismissGroupRequest
+	if err := c.BindJSON(&req); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, ret := gorm.GroupInfoService.DismissGroup(req.GroupId)
+	JsonBack(c, message, ret, nil)
+}
+
+// GetGroupInfo 获取群聊详情
+func GetGroupInfo(c *gin.Context) {
+	var req request.GetGroupInfoRequest
+	if err := c.BindJSON(&req); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, groupInfo, ret := gorm.GroupInfoService.GetGroupInfo(req.GroupId)
+	JsonBack(c, message, ret, groupInfo)
+}
+
+// UpdateGroupInfo 更新群聊消息
+func UpdateGroupInfo(c *gin.Context) {
+	var req request.UpdateGroupInfoRequest
+	if err := c.BindJSON(&req); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, ret := gorm.GroupInfoService.UpdateGroupInfo(req)
+	JsonBack(c, message, ret, nil)
+}
+
+// GetGroupMemberList 获取群聊成员列表
+func GetGroupMemberList(c *gin.Context) {
+	var req request.GetGroupMemberListRequest
+	if err := c.BindJSON(&req); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, groupMemberList, ret := gorm.GroupInfoService.GetGroupMemberList(req.GroupId)
+	JsonBack(c, message, ret, groupMemberList)
+}
+
+// RemoveGroupMembers 移除群聊成员
+func RemoveGroupMembers(c *gin.Context) {
+	var req request.RemoveGroupMembersRequest
+	if err := c.BindJSON(&req); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, ret := gorm.GroupInfoService.RemoveGroupMembers(req)
+	JsonBack(c, message, ret, nil)
+}
